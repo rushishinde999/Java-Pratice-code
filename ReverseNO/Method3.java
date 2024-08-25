43c4e6a3d55ec361d2682d2429b3db58184305b4
rushishinde999 <rushikeshrs2002@gmail.com>
2024-08-25 17:27:02 +0530

    reverse no and string code added

--- ReverseNO/Method3.java
+++ ReverseNO/Method3.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 
 public class Method3 {
   // using String StringBuilder
+  // NUmber as well as String Reverse::
   public static void main(String[] args) {
-    System.out.println("Enter the  Number is :");
+    System.out.println("Enter the  String is :");
     Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
-    StringBuilder s = new StringBuilder();
-    s.append(num);
+    String str = sc.next();
 
+    System.out.println("Enter the number: ");
+
+    Scanner no = new Scanner(System.in);
+    int n = no.nextInt();
+
+    StringBuilder s = new StringBuilder();
+    StringBuilder s1 = new StringBuilder();
+
+    s.append(str);
+    s1.append(n);
+
+    StringBuilder re = s1.reverse();
+    System.out.println("reverse no is : " + re);
     StringBuilder rev = s.reverse();
-    System.out.println("reverse no is" + rev);
+    System.out.println("reverse string is :" + rev);
   }
 }
